31f6c3b2277407bb6c7b49b7e64cb0b4b1a910e4
Ferkhad Kurbanov <hey.ferh@gmail.com>
2020-11-01 01:40:54 +0300

    [groups] fix group entity

--- src/main/java/com/virtuallab/group/dataprovider/GroupEntityRepository.java
+++ src/main/java/com/virtuallab/group/dataprovider/GroupEntityRepository.java
@@ -2,12 +2,13 @@ package com.virtuallab.group.dataprovider;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GroupEntityRepository extends JpaRepository<GroupEntity, String> {
 
     @Query(value = "select g.* from groups g inner join user_group ug on g.uuid = ug.group_uuid where ug.user_uuid = :userId", nativeQuery = true)
-    List<GroupEntity> findGroupsForUser(String userId);
+    List<GroupEntity> findGroupsForUser(@Param("userId") String userId);
 
 }
