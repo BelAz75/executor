1d45f79481187cfefa425b312b077973fabf2d98
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2020-11-01 02:48:29 +0300

    [generator] Fix failed test count

--- src/main/java/com/virtuallab/executor/ContainerStarter.java
+++ src/main/java/com/virtuallab/executor/ContainerStarter.java
@@ -114,7 +114,7 @@ public class ContainerStarter {
         for (int i = 0; i < split.length; i++) {
             if (split[i].contains("Passed")) {
                 passedTests++;
-            } else {
+            } else if (split[i].contains("Failed")) {
                 failedTests++;
             }
         }
