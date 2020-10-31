5aa6fe176fe478980740eaa5d8ace24d222ede60
anleonov <anton.leonov@clausematch.com>
2020-11-01 01:11:07 +0300

    [assign] Add groups to assign query

--- src/main/java/com/virtuallab/task/dataprovider/TaskRepository.java
+++ src/main/java/com/virtuallab/task/dataprovider/TaskRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Set;
+
 public interface TaskRepository extends JpaRepository<TaskEntity, String> {
 
     Page<TaskEntity> findAllByCreatedBy(String createdBy, Pageable pageable);
@@ -15,6 +17,13 @@ public interface TaskRepository extends JpaRepository<TaskEntity, String> {
         countQuery = "select count(1) from task t inner join task_assignment ta on t.uuid = ta.task_uuid where ta.user_uuid = :userId",
         nativeQuery = true
     )
-    Page<TaskEntity> findAssignedTasks(@Param("userId") String userId, Pageable pageable);
+    Page<TaskEntity> findAssignedTasksForUser(@Param("userId") String userId, Pageable pageable);
+
+    @Query(
+        value = "select t.* from task t inner join task_assignment ta on t.uuid = ta.task_uuid where ta.user_uuid = :userId or ta.group_uuid in (:groupIds)",
+        countQuery = "select count(1) from task t inner join task_assignment ta on t.uuid = ta.task_uuid where ta.user_uuid = :userId or ta.group_uuid in (:groupIds)",
+        nativeQuery = true
+    )
+    Page<TaskEntity> findAssignedTasksForUserAndGroups(@Param("userId") String userId, @Param("groupIds") Set<String> groupIds, Pageable pageable);
 
 }
--- src/main/java/com/virtuallab/task/usecase/FindAssignedTasks.java
+++ src/main/java/com/virtuallab/task/usecase/FindAssignedTasks.java
@@ -1,5 +1,7 @@
 package com.virtuallab.task.usecase;
 
+import com.virtuallab.group.dataprovider.GroupEntity;
+import com.virtuallab.group.dataprovider.GroupEntityRepository;
 import com.virtuallab.task.dataprovider.TaskEntity;
 import com.virtuallab.task.dataprovider.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +9,31 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Component
 public class FindAssignedTasks {
 
     private TaskRepository taskRepository;
+    private GroupEntityRepository groupEntityRepository;
 
     @Autowired
-    public FindAssignedTasks(TaskRepository taskRepository) {
+    public FindAssignedTasks(TaskRepository taskRepository, GroupEntityRepository groupEntityRepository) {
         this.taskRepository = taskRepository;
+        this.groupEntityRepository = groupEntityRepository;
     }
 
     public Page<TaskEntity> execute(String userId, int page, int pageSize) {
-        return taskRepository.findAssignedTasks(userId, PageRequest.of(page - 1, pageSize));
+        List<GroupEntity> groupsForUser = groupEntityRepository.findGroupsForUser(userId);
+        Set<String> userGroupsIds = groupsForUser.stream().map(GroupEntity::getId).collect(Collectors.toSet());
+        PageRequest pageRequest = PageRequest.of(page - 1, pageSize);
+        if (userGroupsIds.isEmpty()) {
+            return taskRepository.findAssignedTasksForUser(userId, pageRequest);
+        } else {
+            return taskRepository.findAssignedTasksForUserAndGroups(userId, userGroupsIds, pageRequest);
+        }
     }
 
 }
